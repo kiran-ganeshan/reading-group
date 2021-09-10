8b3e0a4f96e83cfdc19c1b79c1a1f352ed95267d
Kiran Ganeshan <kg23@berkeley.edu>
2021-09-10 15:32:40 -0700

    rework dqn

--- RL/Online RL/Value Iteration Methods/DQN/dqn.py
+++ RL/Online RL/Value Iteration Methods/DQN/dqn.py
@@ -4,20 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from networks import MLP
-
-
-class Critic(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(Critic, self).__init__()
-
-        self.mlp = MLP(input_size=state_dim, 
-                       ouptut_size=action_dim, 
-                       hidden_sizes=(256, 256), 
-                       activation=nn.ReLU)
-
-
-    def forward(self, state, action):
-        return self.mlp(torch.cat([state, action], 1))
     
 
 class DQN(object):
@@ -29,13 +15,17 @@ class DQN(object):
         tau=0.005
     ):
 
-        self.critic = Critic(state_dim, action_dim)
+        self.critic = MLP(input_size=state_dim, 
+                          ouptut_size=action_dim, 
+                          hidden_sizes=(256, 256), 
+                          activation=nn.ReLU)
         self.critic_target = copy.deepcopy(self.critic)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
 
         self.discount = discount
         self.tau = tau
         self.action_dim = action_dim
+        self.one_hot = lambda *x: F.one_hot(*x, num_classes=action_dim)
 
         self.total_it = 0
 
@@ -60,11 +50,10 @@ class DQN(object):
             data = {'target_q': target_Q, **data}
 
         # Get current Q estimates
-        action = F.one_hot(action, num_classes=self.action_dim)
-        current_Q = self.critic(state, action)
+        policy = self.critic(state)
 
         # Compute critic loss
-        critic_loss = F.mse_loss(current_Q, target_Q)
+        critic_loss = F.mse_loss(policy, self.one_hot(action))
         losses = {'critic_loss': critic_loss}
 
         # Optimize the critic
@@ -101,16 +90,9 @@ class DQN(object):
     def save(self, filename):
         torch.save(self.critic.state_dict(), filename + "_critic")
         torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
-        
-        torch.save(self.actor.state_dict(), filename + "_actor")
-        torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
 
 
     def load(self, filename):
         self.critic.load_state_dict(torch.load(filename + "_critic"))
         self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer"))
         self.critic_target = copy.deepcopy(self.critic)
-
-        self.actor.load_state_dict(torch.load(filename + "_actor"))
-        self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
-        self.actor_target = copy.deepcopy(self.actor)
