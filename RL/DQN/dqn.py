d35c8bb292575d4dcb263391b720793dc57e3d2e
Sohom Paul <sohompaul@berkeley.edu>
2021-10-15 19:49:46 -0700

    Revert "attempting to resolve issue where sample only worked for batch_size=None"
    This reverts commit 64b355b345a84959e293ac50efe88c0fd023fc1e.

--- RL/DQN/dqn.py
+++ RL/DQN/dqn.py
@@ -48,15 +48,14 @@ class DQN(object):
         elif len(data) == 6:
             state, action, next_state, reward, not_done, lookahead = data
         else:
-            raise ValueError("Unable to unpack data into form (s, a, s', r, not_done, [lookahead])")
+            raise ValueError("Unable to pack data into form (s, a, s', r, not_done, [lookahead])")
         
         # Compute the target Q value
         with torch.no_grad(): 
             target_Q = self.critic_target(next_state)
             target_Q = torch.max(target_Q, -1)[0]
             data = {'next_q': target_Q}
-            discount = torch.pow(self.discount * torch.ones_like(lookahead), lookahead)
-            target_Q = reward + discount * not_done * target_Q
+            target_Q = reward + pow(self.discount, lookahead) * not_done * target_Q
             data = {'target_q': target_Q, **data}
             
         # Get current Q estimates
